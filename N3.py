dbdab7df231eea86f9cf45a96046050a1282b7be
Xavi-phil <1455178829.qq.com>
2019-11-06 14:39:03 +0800

    12

---
+++ N3.py
@@ -0,0 +1,24 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        from collections import defaultdict
+        lookup = defaultdict(int)
+        start = 0
+        end = 0
+        max_len = 0
+        counter = 0
+        while end < len(s):
+            if lookup[s[end]] > 0:
+                counter += 1
+            lookup[s[end]] += 1
+            end += 1
+            while counter > 0:
+                if lookup[s[start]] > 1:
+                    counter -= 1
+                lookup[s[start]] -= 1
+                start += 1
+            max_len = max(max_len, end - start)
+        return max_len
